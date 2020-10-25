60babca86102db47261e2509dc82c7ffee88202b
Raul Oviedo <antoniorah@gmail.com>
2020-10-25 15:30:44 -0400

    Ability to make request and render NASA mars rover data

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+from local_settings import *
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/mars_rover')
+def mars_rover_photos():
+    url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=%s' % api_key
+    r = requests.get(url = url, params = {})
+    data = r.json() 
+    print('data', data)
+    return render_template('index.html', name=data['photos'])
 
 if __name__ == '__main__':
     app.run(debug=True)
